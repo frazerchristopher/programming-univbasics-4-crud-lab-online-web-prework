1b12d745d772c5de0f7ac9b08d79e373c6318c71
Christopher L Frazer <frazerchristopher@outlook.com>
2019-09-04 05:51:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,6 +1,5 @@
 def create_an_empty_array
-  create_an_empty_array
-  []
+ [] 
 end
 
 def create_an_array
@@ -8,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ <<shovel
 end
 
 def add_element_to_start_of_array(array, element)
